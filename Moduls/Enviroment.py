c2ade9bd7a9a13d0544affb9a308883636c08a5f
ho323 <8536048@gmail.com>
2021-10-01 18:04:39 +0900

    update Enviroment.py

--- Moduls/Enviroment.py
+++ Moduls/Enviroment.py
@@ -1,3 +1,15 @@
+'''
+속성
+chart_data: 주식 종목의 차트 데이터
+observation: 현재 관측치
+idx: 차트 데이터에서의 현재 위치
+
+함수
+reset(): idx와 observation을 초기화
+observe(): idx를 다음 위치로 이동하고 observation을 업데이트
+get_price(): 현재 observation에서 종가를 휙득
+'''
+
 class Enviroment:
     PRICE_IDX = 4  # 종가의 위치
 
@@ -9,7 +21,7 @@ class Enviroment:
     def reset(self):
         self.observation = None
         self.idx = -1
-
+    
     def observe(self):
         if len(self.chart_data) > self.idx + 1:
             self.idx += 1
