06153c510edff5ed80be71706a985c5d83b36edf
ho323 <8536048@gmail.com>
2021-10-03 18:37:47 +0900

    update agent.py

--- Moduls/agent.py
+++ Moduls/agent.py
@@ -30,12 +30,13 @@ import utils
 
 class Agent:
     # 에이전트 상태가 구성하는 값 개수
-    STATE_DIM = 2  # 주식 보유 비율, 포트폴리오 가치 비율
+    STATE_DIM = 3  # 주식 보유 비율, 포트폴리오 가치 비율, 평균 매수 단가 대비 등락률
 
     # 매매 수수료 및 세금
-    TRADING_CHARGE = 0.00015  # 거래 수수료 (일반적으로 0.015%)
-    TRADING_TAX = 0.0025  # 거래세 (실제 0.25%)
+    TRADING_CHARGE = 0.00015  # 거래 수수료 0.015%
+    # TRADING_CHARGE = 0.00011  # 거래 수수료 0.011%
     # TRADING_CHARGE = 0  # 거래 수수료 미적용
+    TRADING_TAX = 0.0025  # 거래세 0.25%
     # TRADING_TAX = 0  # 거래세 미적용
 
     # 행동
@@ -43,11 +44,11 @@ class Agent:
     ACTION_SELL = 1  # 매도
     ACTION_HOLD = 2  # 홀딩
     # 인공 신경망에서 확률을 구할 행동들
-    ACTIONS = [ACTION_BUY, ACTION_SELL]
+    ACTIONS = [ACTION_BUY, ACTION_SELL, ACTION_HOLD]
     NUM_ACTIONS = len(ACTIONS)  # 인공 신경망에서 고려할 출력값의 개수
 
     def __init__(
-        self, environment, balance, min_trading_unit=1, max_trading_unit=2, delayed_reward_threshold=.05):
+        self, environment, balance, min_trading_unit=1, max_trading_unit=2):
         # Environment 객체
         # 현재 주식 가격을 가져오기 위해 환경 참조
         self.environment = environment
@@ -55,28 +56,25 @@ class Agent:
         # 최소 매매 단위, 최대 매매 단위, 지연보상 임계치
         self.min_trading_unit = min_trading_unit  # 최소 단일 거래 단위
         self.max_trading_unit = max_trading_unit  # 최대 단일 거래 단위
-        # 지연보상 임계치
-        ##self.delayed_reward_threshold = delayed_reward_threshold
 
         # Agent 클래스의 속성
         self.initial_balance = balance  # 초기 자본금
         self.balance = balance  # 현재 현금 잔고
         self.num_stocks = 0  # 보유 주식 수
         # 포트폴리오 가치: balance + num_stocks * {현재 주식 가격}
-        self.portfolio_value = 0
+        self.portfolio_value = 0 
         self.base_portfolio_value = 0  # 직전 학습 시점의 PV
         self.num_buy = 0  # 매수 횟수
         self.num_sell = 0  # 매도 횟수
         self.num_hold = 0  # 홀딩 횟수
         self.immediate_reward = 0  # 즉시 보상
-        self.profitloss = 0  # 현새 손익
-        self.base_portfolio = 0  # 직전 지연 보상 이후 손익
+        self.profitloss = 0  # 현재 손익
+        self.base_profitloss = 0  # 직전 지연 보상 이후 손익
         self.exploration_base = 0  # 탐험 행동 결정 기준
 
         # Agent 클래스의 상태
         self.ratio_hold = 0  # 주식 보유 비율
         self.ratio_portfolio_value = 0  # 포트폴리오 가치 비율
-        self.avg_buy_price = 0  # 주당 매수 단가
 
     def reset(self):
         self.balance = self.initial_balance
@@ -151,7 +149,7 @@ class Agent:
                 1 + self.TRADING_CHARGE) * self.min_trading_unit:
                 return False
         elif action == Agent.ACTION_SELL:
-            # 주식 잔고가 있는지 확인
+            # 주식 잔고가 있는지 확인 
             if self.num_stocks <= 0:
                 return False
         return True
@@ -160,9 +158,10 @@ class Agent:
         if np.isnan(confidence):
             return self.min_trading_unit
         added_trading = max(min(
-            int(confidence * (self.max_trading_unit - self.min_trading_unit)
-            ), self.max_trading_unit - self.min_trading_unit
-            ), 0)
+            int(confidence * (self.max_trading_unit - 
+                self.min_trading_unit)),
+            self.max_trading_unit-self.min_trading_unit
+        ), 0)
         return self.min_trading_unit + added_trading
 
     def act(self, action, confidence):
@@ -187,15 +186,15 @@ class Agent:
                 trading_unit = max(
                     min(
                         int(self.balance / (
-                            curr_price * (1 + self.TRADING_CHARGE))), 
+                            curr_price * (1 + self.TRADING_CHARGE))),
                         self.max_trading_unit
-                    ), 
+                    ),
                     self.min_trading_unit
                 )
-            # 수수료를 적용해 총 매수 금액 산정
-            invest_amount = curr_price * (1 + self.TRADING_CHARGE) \
-                * trading_unit
+            # 수수료를 적용하여 총 매수 금액 산정
+            invest_amount = curr_price * (1 + self.TRADING_CHARGE) * trading_unit
             if invest_amount > 0:
+                self.avg_buy_price = (self.avg_buy_price * self.num_stocks + curr_price) / (self.num_stocks + trading_unit)  # 주당 매수 단가 갱신
                 self.balance -= invest_amount  # 보유 현금을 갱신
                 self.num_stocks += trading_unit  # 보유 주식 수를 갱신
                 self.num_buy += 1  # 매수 횟수 증가
